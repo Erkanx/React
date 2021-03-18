376422de41cfd384243f40e9ab3485d0d657cb8a
Erkan Ozcan <erkanozcan@Erkans-MacBook-Pro.local>
2021-03-18 14:40:09 -0500

    Step 1 into React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+
+    }
+  }
+
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
+  }
+
+  render() {
+    return (
+      <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        {
+          this.state.monsters.map(monsters => <h1 key={monsters.id}> {monsters.name} </h1>)
+        }
+      </div>
+    );
+
+  }
 }
 
 export default App;
+{/* <header className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+<p>
+  Edit <code>src/App.js</code> and save to reload.
+</p>
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
+</header>
+</div>
+); */}
